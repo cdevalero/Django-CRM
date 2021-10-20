8eab38e6e0c35c3474d8832f7adbae93afd484f5
cdevalero <onvalero@gmail.com>
2021-10-20 15:52:54 -0400

    add login template

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from user.views import test
-from sales.views import createService
+from sales.views import createService, service, viewService
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', test, name='test'),
     path('createService/', createService, name='createService'),
+    path('service/', service, name='service'),
+    path('viewService/', viewService, name='viewService'),
 ]
--- sales/views.py
+++ sales/views.py
@@ -4,3 +4,9 @@ from .forms import FormService
 def createService(request):
     context = FormService()
     return render(request, 'services/create_service.html', {'context': context})
+
+def service(request):
+    return render(request, 'services/services.html')
+
+def viewService(request):
+    return render(request, 'services/consult_service.html')
